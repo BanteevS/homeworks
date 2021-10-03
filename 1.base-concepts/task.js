958eee3588c512c45e0566cce36a5bbeee5ac854
BanteevS <banteevsl@rambler.ru>
2021-10-03 20:34:39 +0300

    task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,16 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  "use strict";
+  let d = Math.pow(b,2) - (4 * a * c);
+    if (d < 0) {
+        arr = [];
+    } else if (d === 0) {
+        arr = [-(b/(2*a))]
+    } else {
+        let x1 = (-b + Math.sqrt(d)) / (2*a);
+        let x2 = (-b - Math.sqrt(d)) / (2*a);
+        arr = [x1, x2];
+    }
   return arr; // array
 }
 
