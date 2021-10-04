8c54c24a84df83e70847cf101859a42c401b233f
BanteevS <banteevsl@rambler.ru>
2021-10-04 19:53:16 +0300

    task4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = marks;
+  } else {
+    this.marks.concat(marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let avg;
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  if (this.marks.length > 0) {
+    avg = (sum / this.marks.length);
+  } 
+  return avg;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
