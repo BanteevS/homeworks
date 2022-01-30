fe345881f4c80b54122501ac78318f3f3d3e3e3e
BanteevS <banteevsl@rambler.ru>
2022-01-30 21:14:17 +0300

    tasks for decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,62 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(",");
+    let idx = cache.findIndex((item) => item.hash === hash);
+    console.log(idx);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args);
+      cache.push({ hash: hash, value: result });
+      if (cache.length > 5) {
+        cache.shift(0);
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let flag = false;
+  //let count = 0;
+  let timeout;
+  return function (...args) {
+    //count++;
+    if (flag) {
+      //console.log("Вызов номер " + count + " проигнорирован");
+      clearTimeout(timeout);
+      timeout = setTimeout(() => (flag = false), ms);
+      return;
+    } else {
+      func.apply(this, args);
+      flag = true;
+      //console.log("Вызван на попытке номер " + count);
+      clearTimeout(timeout);
+      timeout = setTimeout(() => (flag = false), ms);
+    }
+  };
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let flag = false;
+  let count = 0;
+  let timeout;
+  return function (...args) {
+    count++;
+    console.log("Вызов номер - " + count);
+    if (flag) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => (flag = false), ms);
+      return;
+    } else {
+      func.apply(this, args);
+      flag = true;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => (flag = false), ms);
+    }
+  };
 }
